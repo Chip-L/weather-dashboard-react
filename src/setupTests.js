e0fc203e78276912faf932ba21cdfb39297015b0
Chip Long <chip_long@yahoo.com>
2021-07-22 17:39:45 -0600

    did some initial test setup for app.js and made some notes and configured setupTests so I can start testing properly

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+
+// If your app uses a browser API that you need to mock in your tests or if you need a global setup before running your tests, add a src/setupTests.js to your project. It will be automatically executed before running your tests. (https://create-react-app.dev/docs/running-tests/)
+import "@testing-library/jest-dom";
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
