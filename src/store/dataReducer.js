e07df7ae57e45bad26be4f09752175cd20a018eb
Chip Long <chip_long@yahoo.com>
2021-07-23 21:19:43 -0600

    Filled out more of the reducer

--- src/store/dataReducer.js
+++ src/store/dataReducer.js
@@ -2,7 +2,7 @@ import { useReducer } from "react";
 import {
   GET_SEARCH_CITY,
   SET_CITY,
-  SET_DATA,
+  SET_WEATHER,
   SET_IS_LOADING,
   SET_ERROR,
 } from "./actions";
@@ -10,14 +10,33 @@ import {
 export const reducer = (state, action) => {
   switch (action.type) {
     case GET_SEARCH_CITY:
-      console.log("userReducer: SET_SEARCH_CITY");
+      console.log("userReducer: GET_SEARCH_CITY");
       break;
     case SET_CITY:
       console.log("userReducer: SET_SEARCH_CITY:", action.payload);
       return {
         ...state,
+        isLoading: false,
         city: action.payload,
       };
+    case SET_IS_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case SET_WEATHER:
+      return {
+        ...state,
+        isLoading: false,
+        weather: action.payload,
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        isError: action.payload,
+      };
+
     default:
       return state;
   }
