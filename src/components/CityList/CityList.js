42f6f6cbb9e7b1665fdeb1325787e05c754a9702
Chip Long <chip_long@yahoo.com>
2021-07-24 11:47:55 -0600

    buttons now display, work and are formatted for citylist component

--- src/components/CityList/CityList.js
+++ src/components/CityList/CityList.js
@@ -1,9 +1,27 @@
 import { useEffect } from "react";
-import { setCityList } from "../../store/actions";
+import { setCity, setCityList } from "../../store/actions";
 import { useStoreContext } from "../../store/GlobalState";
 
 function CityList() {
-  const styles = {};
+  const styles = {
+    container: {
+      display: "grid",
+      gridGap: ".5rem",
+      margin: ".5rem",
+      marginTop: "1rem",
+    },
+    button: {
+      fontSize: "1rem",
+      fontWeight: 400,
+      lineHeight: "1.5rem",
+      color: "#fff",
+      backgroundColor: "#6c757d",
+      border: "1px solid #6c757d",
+      borderRadius: ".25rem",
+      marginBottom: "1rem",
+      padding: ".375rem .75rem",
+    },
+  };
 
   const [{ cityList, city }, dispatch] = useStoreContext();
 
@@ -14,31 +32,30 @@ function CityList() {
     const storageKey = "weatherCityList";
     const maxInHistoryList = 10;
 
-    if (!city.name) return;
-
     // get the list from local storage
     const storedList = JSON.parse(localStorage.getItem(storageKey)) || [];
     console.log(storedList);
 
-    // get the index of the city to see if it is already in the list and remove it if it is
-    const currentIndex = storedList.findIndex(
-      (storedCity) => storedCity.name === city.name
-    );
-    if (currentIndex > -1) {
-      storedList.splice(currentIndex, 1);
+    if (city.name) {
+      // get the index of the city to see if it is already in the list and remove it if it is
+      const currentIndex = storedList.findIndex(
+        (storedCity) => storedCity.name === city.name
+      );
+      if (currentIndex > -1) {
+        storedList.splice(currentIndex, 1);
+      }
+
+      // add city to the front of the array
+      storedList.unshift(city);
+
+      // set the size of the list
+      if (storedList.length > maxInHistoryList) {
+        storedList.length = maxInHistoryList;
+      }
+
+      // put back in local storage
+      localStorage.setItem(storageKey, JSON.stringify(storedList));
     }
-
-    // add city to the front of the array
-    storedList.unshift(city);
-
-    // set the size of the list
-    if (storedList.length > maxInHistoryList) {
-      storedList.length = maxInHistoryList;
-    }
-
-    // put back in local storage
-    localStorage.setItem(storageKey, JSON.stringify(storedList));
-
     // update the cityList state
     dispatch(setCityList(storedList));
   }, [city]);
@@ -46,7 +63,19 @@ function CityList() {
   if (!cityList.length) {
     return <div></div>;
   } else {
-    return <div>{/* //add buttons here */}</div>;
+    return (
+      <div style={styles.container}>
+        {cityList.map((storedCity) => (
+          <button
+            key={storedCity.name}
+            onClick={() => dispatch(setCity(storedCity))}
+            style={styles.button}
+          >
+            {storedCity.name}
+          </button>
+        ))}
+      </div>
+    );
   }
 }
 
